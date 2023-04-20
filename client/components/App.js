03de06ebf8673956464d75f58e6dafd4ce6a421b
Ry G <Unstoppable@Unstoppable.local>
2023-04-20 12:28:13 -0700

    successfully reading file selected from users machine

--- client/components/App.js
+++ client/components/App.js
@@ -4,16 +4,17 @@ import Stack from './Stack';
 import Task from './Task';
 import MicroTask from './MicroTask';
 
-const initialState = {
-  
-};
-
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      fileContent: ''
+    }
   }
 
   componentDidMount() {
+    
+    console.log('state from componentDidMount: ', this.state);
   
     const parseJS = function() {
       //parse through code
@@ -37,26 +38,58 @@ class App extends Component {
 
       postResquest(newRow);
     }
+
   }
 
   render() {
+    console.log('APP rendered');
+    let fileContent = '';
+
+    async function writeFile() {
+      const content = document.getElementById("fileContent");
+      const [file] = document.querySelector("input[type=file]").files;
+      const reader = new FileReader();
+    
+      reader.addEventListener(
+        "load",
+        () => {
+          // this will then display a text file
+          content.innerText = file.name;
+          fileContent = reader.result;
+          console.log('fileContent read: \n', fileContent);
+        },
+        false
+      );
+    
+      if (file) {
+        await reader.readAsText(file);
+      }
+    };
+
+    if(this.state.fileContent !== fileContent) {
+      this.setState({fileContent: fileContent});
+      console.log('state as seen from inside render(): ', this.state);
+    };
+    
+
     return(
       <div className='app'>
 
-        <div id='stackTitle' class="title">CALL STACK</div>
+        <div id='stackTitle' className="title">CALL STACK</div>
 
         <Stack />
 
-        <div id="selectFileTitle" class="title">SELECT JAVASCRIPT FILE</div>
+        <div id="selectFileTitle" className="title">SELECT JAVASCRIPT FILE</div>
 
         <div id='selectFileContainer' className='container'>
-          <button id="selectFileBtn" class="btn" >Select File</button>
+          <input type="file" onChange={writeFile} /><br />
+          <p id="fileContent">No file chosen</p>
         </div>
 
-        <div id="runBtnTitle" class="title">RUN VISUALIZER</div>
+        <div id="runBtnTitle" className="title">RUN VISUALIZER</div>
 
         <div id='runContainer' className='container'>
-          <button id="runBtn" class="btn" >Run</button>
+          <button id="runBtn" className="btn" >Run</button>
         </div>
 
         <div id="exConTitle" className='title'>EXECUTION CONTEXT</div>
@@ -67,14 +100,14 @@ class App extends Component {
 
         <div id="microTaskTitle" className='title'>MICROTASK QUEUE</div>
 
-        <div id="microTaskQueue" class="queue">
+        <div id="microTaskQueue" className="queue">
           <MicroTask />
           <MicroTask />
           <MicroTask />
         </div>
 
         <div id="taskTitle" className='title'>TASK QUEUE</div>
-        <div id="taskQueue" class="queue">
+        <div id="taskQueue" className="queue">
           <Task />
           <Task />
           <Task />
